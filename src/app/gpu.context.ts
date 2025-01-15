240b5ed19b26dddf9f1620e00d8f3e1a3056bf20
Matthias Hochpöchler <matthias.hochpoechler3d@gmail.com>
2025-01-15 19:47:04 +0100

    small changes

---
+++ src/app/gpu.context.ts
@@ -0,0 +1,38 @@
+export class GpuContext {
+  public canvas?: HTMLCanvasElement;
+  public context?: any;
+  public canvasFormat?: any;
+  public device?: any;
+
+  constructor() {
+    this.setup();
+  }
+
+  public async setup() {
+    if (!window.navigator.gpu) {
+      throw new Error("WebGPU not supported on this browser.");
+    }
+
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      throw new Error("No appropriate GPUAdapter found.");
+    }
+
+    this.canvas = document.querySelector("canvas")!!;
+    this.device = await adapter.requestDevice();
+    this.context = this.canvas.getContext("webgpu");
+    this.canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    this.context!!.configure({
+      device: this.device,
+      format: this.canvasFormat,
+    });
+  }
+
+  public createStorageBuffer(label: string, byteLength: number) {
+    return this.device.createBuffer({
+        label: label,
+        size: byteLength,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+      })
+  }
+}
---
+++ src/app/gpu.render.ts
@@ -0,0 +1,76 @@
+
+import { GpuContext } from './gpu.context';
+import { Square } from './utils'
+
+export interface ShapeInstances {
+    shape: Square,
+    instances: number
+}
+
+export class Render {
+
+    private renderPipeline?: any;
+    private bindGroups?: any;
+
+    private shape: Square; // TODO think of refactoring this
+    private instances: number;
+
+    constructor(
+        gpuContext: GpuContext,
+        pipelineLayout: any,
+        bindGroups: any,
+        vertexShaderSource: any,
+        fragmentShaderSource: any,
+        shape: Square,
+        instances: number
+    ) {
+        this.bindGroups = bindGroups;
+        this.shape = shape;
+        this.instances = instances;
+
+        // Create a pipeline that renders the cell.
+        this.renderPipeline = gpuContext.device.createRenderPipeline({
+            label: "Cell pipeline",
+            layout: pipelineLayout,
+            vertex: {
+                module: gpuContext.device.createShaderModule({
+                    code: vertexShaderSource,
+                }),
+                entryPoint: "vertexMain",
+                buffers: [this.shape.vertexBufferLayout]
+            },
+            fragment: {
+                module: gpuContext.device.createShaderModule({
+                    code: fragmentShaderSource,
+                }),
+                entryPoint: "fragmentMain",
+                targets: [{
+                    format: gpuContext.canvasFormat
+                }]
+            }
+        });
+
+    }
+
+    execute(encoder: any, step: number, view: any) {
+        const pass = encoder.beginRenderPass({
+            colorAttachments: [{
+                view: view,
+                loadOp: "clear",
+                clearValue: { r: 0, g: 0, b: 0, a: 1.0 },
+                storeOp: "store",
+            }]
+        });
+
+        pass.setPipeline(this.renderPipeline);
+        pass.setBindGroup(0, this.bindGroups[step % 2]);
+
+        pass.setVertexBuffer(0, this.shape.vertexBuffer);
+            pass.draw(
+                this.shape.vertices.length / 2, // number of vertices
+                this.instances, // number of instances
+            );
+
+        pass.end();
+    }
+}
