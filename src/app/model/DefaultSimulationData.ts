4bacad3db862abe25c20596398d5ccbbc8b433d2
Matthias Hochpöchler <matthias.hochpoechler3d@gmail.com>
2025-01-17 09:01:08 +0100

    new defaults

--- src/app/model/DefaultSimulationData.ts
+++ src/app/model/DefaultSimulationData.ts
@@ -1,7 +1,7 @@
 import { create, ParticleType, Vec4 } from "./Point";
 import { Force, SimulationData } from "./Simulation";
 
-export const RED = new ParticleType("Red", 0, new Vec4(1, 0, 0, 1), 80);
+export const RED = new ParticleType("Red", 0, new Vec4(1, 0, 0, 1), 500);
 export const BLUE = new ParticleType("Blue", 1, new Vec4(0, 0, 1, 1), 80);
 export const YELLOW = new ParticleType("Yellow", 2, new Vec4(1, 1, 0, 1), 80);
 export const GREEN = new ParticleType("Green", 3, new Vec4(0, 1, 0, 1), 80);
@@ -15,10 +15,10 @@ export function createDefaultSimulationModel() {
   data.forceByType = new Map()
 
   data.forceByType.set(RED, [
-    new Force(RED, RED, 2),
-    new Force(RED, BLUE, 0.5),
-    new Force(RED, YELLOW, -2),
-    new Force(RED, GREEN, 2),
+    new Force(RED, RED, 1),
+    new Force(RED, BLUE, 0.25),
+    new Force(RED, YELLOW, 0.25),
+    new Force(RED, GREEN, -0.5),
   ]);
 
   data.forceByType.set(BLUE, [
@@ -32,20 +32,20 @@ export function createDefaultSimulationModel() {
     new Force(YELLOW, RED, 0.5),
     new Force(YELLOW, BLUE, 1.2),
     new Force(YELLOW, YELLOW, -0.66),
-    new Force(YELLOW, GREEN, 0.1),
+    new Force(YELLOW, GREEN, 0.5),
   ]);
 
   data.forceByType.set(GREEN, [
     new Force(GREEN, RED, 0.25),
     new Force(GREEN, BLUE, -0.25),
     new Force(GREEN, YELLOW, -0.25),
-    new Force(GREEN, GREEN, 0.25),
+    new Force(GREEN, GREEN, -0.25),
   ]);
 
-  data.points = create(3000, RED)
-    .concat(create(3000, BLUE))
-    .concat(create(3000, YELLOW))
-    .concat(create(3000, GREEN));
+  data.points = create(4000, RED)
+    .concat(create(4000, BLUE))
+    .concat(create(4000, YELLOW))
+    .concat(create(4000, GREEN));
 
   return data;
 }
