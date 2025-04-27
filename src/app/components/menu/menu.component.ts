1376fa6111f2e2627bfbb40ad8094c7b04b35039
Matthias Hochpöchler <matthias.hochpoechler3d@gmail.com>
2025-04-27 18:29:23 +0200

    add forces matrix

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,7 +3,6 @@ import {TabsModule} from 'primeng/tabs';
 import {FormsModule} from "@angular/forms";
 import {Color, create, ParticleType, Point} from "../../../scene/model/Point";
 import {randomRounded} from "../utils/utils";
-import {vec4, vec3} from "gl-matrix";
 import {DataStore} from "../../store/data.store";
 import {DataService} from "../../services/data.service";
 import {SliderModule} from 'primeng/slider';
@@ -14,123 +13,157 @@ import {ChipModule} from 'primeng/chip';
 import {FileSelectEvent, FileUploadModule} from 'primeng/fileupload';
 import {SimulationData} from "../../../scene/model/Simulation";
 import {readFile} from "../utils/file-utils";
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { FieldsetModule } from 'primeng/fieldset';
-import { ColorPickerModule } from 'primeng/colorpicker';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {FieldsetModule} from 'primeng/fieldset';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import {KnobModule} from 'primeng/knob';
+import {TableModule} from 'primeng/table';
+
 
 @Component({
-  selector: 'app-menu',
-  imports: [ColorPickerModule, FieldsetModule, InputTextModule, ButtonModule, FileUploadModule, ChipModule, TabsModule, FormsModule, SliderModule, InputNumberModule, FloatLabelModule],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+    selector: 'app-menu',
+    imports: [TableModule, KnobModule, ColorPickerModule, FieldsetModule, InputTextModule, ButtonModule, FileUploadModule, ChipModule, TabsModule, FormsModule, SliderModule, InputNumberModule, FloatLabelModule],
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
 
-  @Input() public scene: ParticleSimulation = new ParticleSimulation();
-  public pointNumber: number = 500;
-
-  constructor(public dataStore: DataStore, public dataService: DataService) {
-  }
-
-  public createRandomWorld() {
-    let newPoints: Point[] = [];
-    this.dataStore.simulationData.physicsData.types.forEach((type, index) => {
-      newPoints.push(...create(this.pointNumber, type, 200));
-    })
-    this.dataStore.simulationData.points = newPoints;
-
-    this.scene.setScene(
-        this.dataStore.simulationData.physicsData,
-        this.dataStore.simulationData.points
-    );
-  }
-
-  randomForces() {
-    this.dataStore.simulationData.physicsData.types.forEach((type, rowIndex) => {
-      this.dataStore.simulationData.physicsData.types.forEach((col, colIndex) => {
-        this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] = randomRounded(-0.5, 0.5);
-      })
-    });
-
-    // update scene values
-    this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
-  }
-
-  randomParticleValues() {
-    this.dataStore.simulationData.physicsData.types.forEach((type, rowIndex) => {
-      type.radius = randomRounded(0, 50);
-      type.mass = randomRounded(0, 0.25);
-      type.size = randomRounded(0.25, 1)
-      type.color = new Color(
-          randomRounded(0, 255),
-          randomRounded(0, 255),
-          randomRounded(0, 255)
-      )
-    });
-
-    // update scene values
-    this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
-  }
-
-  multiplyForces(factor: number) {
-    this.dataStore.simulationData.physicsData.forces.forEach((row, rowIndex) => {
-      row.forEach((col, colIndex) => {
-        this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] *= factor;
-      })
-    })
-
-    // update scene values
-    this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
-  }
-
-  async saveSimulation() {
-    const sim = this.dataStore.simulationData;
-    sim.points = await this.scene.getCurrentPoints();
-    await this.dataService.saveSimulation(sim);
-  }
-
-  async selectScene(event: FileSelectEvent) {
-    this.dataStore.simulationData = await readFile<SimulationData>(event);
-    this.scene.setScene(
-        this.dataStore.simulationData.physicsData,
-        this.dataStore.simulationData.points
-    )
-  }
-
-  updatePhysics() {
-    this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
-  }
-
-  addType() {
-    let length = this.dataStore.simulationData.physicsData.types.length;
-    let newType = new ParticleType(
-        "New Type " + length,
-        length,
-        new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255),
-        Math.random() * 30 + 10,
-        Math.random() * 1 + 0.5,
-        Math.random()
-    );
-
-    this.dataStore.simulationData.physicsData.types.push(newType);
-    this.dataStore.simulationData.physicsData.forces.push(
-        Array(this.dataStore.simulationData.physicsData.types.length).fill(0) //TODO, update all forces of all particles and set init value
-    );
-  }
-
-  deleteType(index: number) {
-    if (this.dataStore.simulationData.physicsData.types.length > 1) {
-      this.dataStore.simulationData.physicsData.types.splice(index, 1)
-      this.dataStore.simulationData.physicsData.forces.splice(index, 1);
-      this.dataStore.simulationData.physicsData.forces.forEach(row => {
-        row.splice(index, 1);
-      })
+    @Input() public scene: ParticleSimulation = new ParticleSimulation();
+    public pointNumber: number = 500;
+    public LIMITS = {
+        MIN_FORCE: -1,
+        MAX_FORCE: 1,
+        MAX_RADIUS: 50,
+        MAX_MASS: 0.25,
+        MIN_SIZE: 0.25,
+        MAX_SIZE: 1.25
     }
 
-    // regenerate indices
-    this.dataStore.simulationData.physicsData.types.forEach((type, index) => {
-      type.id = index;
-    })
-  }
+    constructor(public dataStore: DataStore, public dataService: DataService) {
+    }
+
+    public createRandomWorld() {
+        let newPoints: Point[] = [];
+        this.dataStore.simulationData.physicsData.types.forEach((type, index) => {
+            newPoints.push(...create(this.pointNumber, type, 200));
+        })
+        this.dataStore.simulationData.points = newPoints;
+
+        this.scene.setScene(
+            this.dataStore.simulationData.physicsData,
+            this.dataStore.simulationData.points
+        );
+    }
+
+    resetForces() {
+        this.dataStore.simulationData.physicsData.types.forEach((type, rowIndex) => {
+            this.dataStore.simulationData.physicsData.types.forEach((col, colIndex) => {
+                this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] = 0;
+            })
+        });
+
+        // update scene values
+        this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
+    }
+
+    randomForces() {
+        this.dataStore.simulationData.physicsData.types.forEach((type, rowIndex) => {
+            this.dataStore.simulationData.physicsData.types.forEach((col, colIndex) => {
+                this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] = randomRounded(this.LIMITS.MIN_FORCE, this.LIMITS.MAX_FORCE);
+            })
+        });
+
+        // update scene values
+        this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
+    }
+
+    randomParticleValues() {
+        this.dataStore.simulationData.physicsData.types.forEach((type, rowIndex) => {
+            type.radius = randomRounded(0, this.LIMITS.MAX_RADIUS);
+            type.mass = randomRounded(0, this.LIMITS.MAX_MASS);
+            type.size = randomRounded(this.LIMITS.MIN_SIZE, this.LIMITS.MAX_SIZE)
+            type.color = new Color(
+                randomRounded(0, 255),
+                randomRounded(0, 255),
+                randomRounded(0, 255)
+            )
+        });
+
+        // update scene values
+        this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
+    }
+
+    multiplyForces(factor: number) {
+        this.dataStore.simulationData.physicsData.forces.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] *= factor;
+                this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex] =
+                    Math.max(
+                        Math.min(
+                            this.dataStore.simulationData.physicsData.forces[rowIndex][colIndex],
+                            this.LIMITS.MAX_FORCE
+                        ),
+                        this.LIMITS.MIN_FORCE
+                    );
+            })
+        })
+
+        // update scene values
+        this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
+    }
+
+    async saveSimulation() {
+        const sim = this.dataStore.simulationData;
+        sim.points = await this.scene.getCurrentPoints();
+        await this.dataService.saveSimulation(sim);
+    }
+
+    async selectScene(event: FileSelectEvent) {
+        this.dataStore.simulationData = await readFile<SimulationData>(event);
+        this.scene.setScene(
+            this.dataStore.simulationData.physicsData,
+            this.dataStore.simulationData.points
+        )
+    }
+
+    updatePhysics() {
+        this.scene.updatePhysics(this.dataStore.simulationData.physicsData);
+    }
+
+    addType() {
+        let length = this.dataStore.simulationData.physicsData.types.length;
+        let newType = new ParticleType(
+            "Particle " + length,
+            length,
+            new Color(
+                randomRounded(0, 255),
+                randomRounded(0, 255),
+                randomRounded(0, 255)
+            ),
+            randomRounded(0, this.LIMITS.MAX_RADIUS),
+            randomRounded(this.LIMITS.MIN_SIZE, this.LIMITS.MAX_SIZE),
+            randomRounded(0, this.LIMITS.MAX_MASS)
+        );
+
+        this.dataStore.simulationData.physicsData.types.push(newType);
+        this.dataStore.simulationData.physicsData.forces.push(
+            Array(this.dataStore.simulationData.physicsData.types.length).fill(0) //TODO, update all forces of all particles and set init value
+        );
+    }
+
+    deleteType(index: number) {
+        if (this.dataStore.simulationData.physicsData.types.length > 1) {
+            this.dataStore.simulationData.physicsData.types.splice(index, 1)
+            this.dataStore.simulationData.physicsData.forces.splice(index, 1);
+            this.dataStore.simulationData.physicsData.forces.forEach(row => {
+                row.splice(index, 1);
+            })
+        }
+
+        // regenerate indices
+        this.dataStore.simulationData.physicsData.types.forEach((type, index) => {
+            type.id = index;
+        })
+    }
 }
